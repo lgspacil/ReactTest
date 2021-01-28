dead7fb9eb3c23dfb8a16790f64af476e455b4f2
lgspacil <lucas.spacil@terraclear.com>
2021-01-27 17:24:57 -0800

    fix shape draw

--- src/map/MapPolygons.tsx
+++ src/map/MapPolygons.tsx
@@ -1,23 +1,37 @@
 import { FeatureCollection } from '@turf/turf';
 import { GeoJSONLayer } from "react-mapbox-gl";
-import React, { useEffect } from 'react';
-import { StoreContainer } from '../store';
+import React, { useContext, useEffect } from 'react';
+import { ShapeContext } from '../hooks/ShapeContext';
+import * as turf from '@turf/turf';
 
 interface Props {
     fc: FeatureCollection | null;
 }
 
 const MapPolygons = (props: Props) => {
-    
-    const store = StoreContainer.useContainer();
+
+    // const store = StoreContainer.useContainer();
+    const { shapeId } = useContext(ShapeContext);
+
+    const newFeatureList = props.fc?.features.filter((feature) => {
+        if (feature.id !== shapeId) {
+            return feature
+        }
+    })
+
+    let trimmedFc: FeatureCollection | undefined;
+    if (newFeatureList) {
+        trimmedFc = turf.featureCollection(newFeatureList);
+    }
+
 
     return (<>
         <GeoJSONLayer
-            data={store.featureCollection}
-            fillPaint={{ "fill-color": 'black', 'fill-opacity': 0.3 }}
-            linePaint={{ 'line-color': 'red', 'line-width': 4 }}
-            // fillOnMouseEnter={(event) => this.onEnteredPolygon(event)}
-            // fillOnMouseLeave={(event) => this.onLeftPolygon(event)}
+            data={trimmedFc ? trimmedFc : props.fc}
+            // fillPaint={{ "fill-color": 'black', 'fill-opacity': 0.3 }}
+            linePaint={{ 'line-color': '#f5b336', 'line-width': 3 }}
+        // fillOnMouseEnter={(event) => this.onEnteredPolygon(event)}
+        // fillOnMouseLeave={(event) => this.onLeftPolygon(event)}
         />
     </>)
 }
