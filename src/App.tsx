82c3c485644d65529fb8955be17f115cadb55a9e
lgspacil <lucas.spacil@terraclear.com>
2021-06-29 13:38:04 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ToolRouter from './routes/router';
 import MiniDrawer from './SideDrawer/MiniDrawer';
 
-// ---------------------- hello -------------
+// ---------------------- hello????? (1) -------------
+
+// new commit comming from branch 1
 
 // Router wrapping both the Drawer and the routes since we want access to use history in the drawer component
 
@@ -11,7 +13,7 @@ const App: React.FunctionComponent<{}> = () => {
 
   return (
     <Router>
-      <MiniDrawer /> 
+      {/* <MiniDrawer />  */}
       <ToolRouter />
     </Router>
   )
