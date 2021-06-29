4fd841ef69be1303054acc40e9c6fd8eb679d626
lgspacil <lucas.spacil@terraclear.com>
2021-06-29 13:14:08 -0700

    unnecessary commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ToolRouter from './routes/router';
 import MiniDrawer from './SideDrawer/MiniDrawer';
 
+console.log('I should not be here ------')
+
 // Router wrapping both the Drawer and the routes since we want access to use history in the drawer component
 
 const App: React.FunctionComponent<{}> = () => {
